d74ed4958411cd6f0d579db8c75299c843b60389
jieunn <jieun776121@gmail.com>
2025-03-06 21:26:08 +0900

    feat : add result api

--- src/pages/Main/Main.js
+++ src/pages/Main/Main.js
@@ -2,19 +2,39 @@ import styled from "styled-components";
 import { useNavigate } from "react-router-dom";
 import Image from "./components/Image";
 import Chat from "./components/Chat";
+import { patchChat } from "./api/MainApi";
+import { useState } from "react";
 
 const Main = () => {
   const navigate = useNavigate();
+  const [chatData, setChatData] = useState(null);
 
-  const handleTakeResult = () => {
-    navigate("/result");
+  const handleTakeResult = async () => {
+    if (!chatData) return;
+
+    try {
+      const memoryId = chatData?.memory_id;
+      const response = await patchChat({ memoryId });
+
+      if (response.url) {
+        navigate("/result", { state: { resultUrl: response.url } });
+      } else {
+        alert("결과 URL이 없습니다.");
+      }
+    } catch (error) {
+      console.error("patchChat 실패:", error);
+      alert("회상 종료 요청에 실패했습니다.");
+    }
   };
 
   return (
     <Container>
-      <Image />
-      <Chat />
-      <ResultBtn onClick={handleTakeResult}>감정 결과 확인</ResultBtn>
+      <LogoImg src="/img/miraeasset.jpg" />
+      <Image onImageUpload={setChatData} />
+      <Chat initialMessage={chatData} isDisabled={!chatData} />
+      <ResultBtn onClick={handleTakeResult} disabled={!chatData}>
+        감정 결과 확인
+      </ResultBtn>
     </Container>
   );
 };
@@ -30,16 +50,21 @@ const Container = styled.div`
   height: 100%;
 `;
 
+const LogoImg = styled.img`
+  width: 200px;
+  height: auto;
+`;
+
 const ResultBtn = styled.button`
   padding: 10px 20px;
-  background-color: #f58220;
+  background-color: ${({ disabled }) => (disabled ? "#ccc" : "#f58220")};
   color: white;
   border: none;
   border-radius: 5px;
-  cursor: pointer;
+  cursor: ${({ disabled }) => (disabled ? "not-allowed" : "pointer")};
   font-size: 16px;
 
   &:hover {
-    background-color: #cb6015;
+    background-color: ${({ disabled }) => (disabled ? "#ccc" : "#cb6015")};
   }
 `;
